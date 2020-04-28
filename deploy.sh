479878fc821e6b51be5e6dad91dc42743cb29ae7
wuxuebin <1477535159@qq.com>
2020-04-28 16:18:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 set -e
 
 # 构建
-npm run build
+#npm run build
 
 # cd 到构建输出的目录下 
-cd dist
+#cd dist
 
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 git remote add origin https://github.com/Wu-XueBin-007/vuedeploy.git
